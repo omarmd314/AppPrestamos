c651dacff2f84d70b21657d59894ab9af7b1e5d7
Omar Meza <omeza3.14@hotmail.com>
2024-07-24 23:06:30 -0500

    Configuracion de la base de datos en sqlite

---
+++ app/src/main/java/com/example/appprestamos/DatabaseHelper.java
@@ -0,0 +1,91 @@
+package com.example.appprestamos;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static String DATABASE_NAME = "mydatabase.db";
+    private static final int DATABASE_VERSION = 1;
+    private String DATABASE_PATH;
+    private Context mContext;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.mContext = context;
+        DATABASE_PATH = context.getDatabasePath(DATABASE_NAME).getPath();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // No se necesita implementar
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // No se necesita implementar
+    }
+
+    public void createDatabase() throws IOException {
+        boolean dbExist = checkDatabase();
+
+        if (!dbExist) {
+            this.getReadableDatabase();
+            this.close();
+            try {
+                copyDatabase();
+            } catch (IOException e) {
+                throw new Error("Error copiando la base de datos");
+            }
+        }
+    }
+
+    private boolean checkDatabase() {
+        SQLiteDatabase checkDB = null;
+        try {
+            String myPath = DATABASE_PATH;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        } catch (SQLiteException e) {
+            // La base de datos no existe aún
+        }
+
+        if (checkDB != null) {
+            checkDB.close();
+        }
+        return checkDB != null;
+    }
+
+    private void copyDatabase() throws IOException {
+        InputStream myInput = mContext.getAssets().open(DATABASE_NAME);
+        OutputStream myOutput = new FileOutputStream(DATABASE_PATH);
+
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+    public SQLiteDatabase openDatabase() throws SQLiteException {
+        return SQLiteDatabase.openDatabase(DATABASE_PATH, null, SQLiteDatabase.OPEN_READWRITE);
+    }
+
+    @Override
+    public synchronized void close() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        if (db != null && db.isOpen()) {
+            db.close();
+        }
+        super.close();
+    }
+}
--- app/src/main/java/com/example/appprestamos/MainActivity.java
+++ app/src/main/java/com/example/appprestamos/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.appprestamos;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +13,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,47 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        dbHelper = new DatabaseHelper(this);
+
+        try {
+            dbHelper.createDatabase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        SQLiteDatabase db = null;
+        try {
+            db = dbHelper.openDatabase();
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+        }
+
+        String[] array =  new String[10];
+        String empleado = "";
+        int i = 0;
+        if (db != null) {
+            Cursor cursor = db.rawQuery("SELECT * FROM tb_cliente", null);
+            if (cursor.moveToFirst()) {
+                do {
+                    @SuppressLint("Range") String nombre = cursor.getString(cursor.getColumnIndex("cli_nombre"));
+                    //@SuppressLint("Range") int age = cursor.getInt(cursor.getColumnIndex("age"));
+                    @SuppressLint("Range") String apellido = cursor.getString(cursor.getColumnIndex("cli_apellido"));
+                    // Haz algo con los datos
+                    //array[i] = name.concat(" " + email);
+                    empleado = empleado.concat(nombre + " " + apellido+ "\n");
+                    //i++;
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+            db.close();
+        }
+
+        Toast.makeText(this, empleado, Toast.LENGTH_LONG).show();
+
+
+
     }
+
+
 }
