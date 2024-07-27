df757f357f91285b83bf46ab26b4ee0a85f7b169
Omar Meza <omeza3.14@hotmail.com>
2024-07-26 23:59:19 -0500

    Se agrego la clase ListAdapter y se modifico la clase ListData

--- app/src/main/java/com/example/appprestamos/ListData.java
+++ app/src/main/java/com/example/appprestamos/ListData.java
@@ -2,12 +2,14 @@ package com.example.appprestamos;
 
 public class ListData {
     String pre_desc, fecha_i, fecha_f, cli_nombre;
+    float pre_monto;
     int pre_id, image;
-    public ListData(int pre_id,String pre_desc,String cli_nombre, String fecha_i,String fecha_f, int image) {
+    public ListData(int pre_id,String pre_desc,String cli_nombre,float pre_monto, String fecha_i,String fecha_f, int image) {
         this.pre_id = pre_id;
         this.pre_desc = pre_desc;
         this.fecha_i = fecha_i;
         this.cli_nombre = cli_nombre;
+        this.pre_monto = pre_monto;
         this.fecha_f = fecha_f;
         this.fecha_i = fecha_i;
         this.image = image;
